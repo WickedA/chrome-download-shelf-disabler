da175d4d6de4da87935668c6eb889ed8fc0bc159
WickedA <wickeda@users.noreply.github.com>
2016-09-14 22:59:10 +0300

    Mac-only version, hopefully functional

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,19 @@
-chrome.downloads.setShelfEnabled(false);
+// The "official" way to see what operating system you're running on is to use chrome.runtime.
+// .getPlatformInfo(), which takes a callback because of course it does.
+function DisableShelf() {
+    chrome.runtime.getPlatformInfo(function(info) {
+        if (info.os === "mac") {
+            chrome.downloads.setShelfEnabled(false)
+        }
+    })
+}
+
+
+// Run it once (when the extension loads, presumably), then add a listener. I'm not sure why it
+// works, but it certainly does.
+
+DisableShelf()
+
 chrome.runtime.onStartup.addListener(function() {
-	chrome.downloads.setShelfEnabled(false);
-});
+    DisableShelf()
+})
